caf3f87d74093b92056822a229b1f15609c0083f
McPotato <luc.sydney.georges@gmail.com>
2020-12-28 11:05:54 +0100

    added git abbrv

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,9 @@
 abbr -a v nvim
+abbr -a ga 'git add .'
+abbr -a gcm 'git commit -m ""'
+abbr -a gc 'git checkout'
+abbr -a gpl 'git pull'
+abbr -a gps 'git push'
 
 set PATH $HOME/.cargo/bin $PATH
 set PATH /Users/mc/Library/Python/3.8/bin $PATH
