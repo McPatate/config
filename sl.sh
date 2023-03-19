82ccb88769b0b73443d9b38f99cec4601efe3115
Luc Georges <luc@huggingface.co>
2023-03-19 17:04:40 +0100

    feat: farewell old nvim conf, welcome lazyvim

--- sl.sh
+++ sl.sh
@@ -7,7 +7,7 @@ mkdir -p ~/.config/nvim
 
 ln -s $CONFIG_HOME/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 # ln -s $CONFIG_HOME/tmux.conf ~/.tmux.conf
-ln -s $CONFIG_HOME/nvim/init.vim ~/.config/nvim/init.vim
+ln -s $CONFIG_HOME/nvim ~/.config/nvim
 # ln -s $CONFIG_HOME/fish/config.fish ~/.config/fish/config.fish
 # ln -s $CONFIG_HOME/kitty/kitty.conf ~/.config/kitty/kitty.conf
 ln -s $CONFIG_HOME/zsh/.zshrc ~/.zshrc
