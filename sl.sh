51a6190909356c098ce4fb3ee2101d7a3f728c3a
Luc Georges <luc@huggingface.co>
2023-03-21 15:19:06 +0100

    feat: update symbolic link script

--- sl.sh
+++ sl.sh
@@ -2,17 +2,16 @@
 
 export CONFIG_HOME=/home/mc/Documents/config
 
-mkdir -p ~/.config/alacritty
-mkdir -p ~/.config/nvim
-
-ln -s $CONFIG_HOME/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-# ln -s $CONFIG_HOME/tmux.conf ~/.tmux.conf
-ln -s $CONFIG_HOME/nvim ~/.config/nvim
+# Platform independant
+ln -s $CONFIG_HOME/alacritty ~/.config/alacritty
+ln -s $CONFIG_HOME/bat ~/.config/bat
 # ln -s $CONFIG_HOME/fish/config.fish ~/.config/fish/config.fish
 # ln -s $CONFIG_HOME/kitty/kitty.conf ~/.config/kitty/kitty.conf
-ln -s $CONFIG_HOME/zsh/.zshrc ~/.zshrc
+ln -s $CONFIG_HOME/nvim ~/.config/nvim
 ln -s $CONFIG_HOME/starship/starship.toml ~/.config/starship.toml
-ln -s $CONFIG_HOME/k9s/mocha.yml ~/.config/k9s/skin.yml
+# ln -s $CONFIG_HOME/tmux.conf ~/.tmux.conf
+ln -s $CONFIG_HOME/zsh/.zshrc ~/.zshrc
+
 # Mac
 # rm ~/Library/Application\ Support/Code/User/settings.json
 # rm ~/Library/Application\ Support/Code/User/keybindings.json
@@ -21,7 +20,9 @@ ln -s $CONFIG_HOME/k9s/mocha.yml ~/.config/k9s/skin.yml
 # defaults write -g KeyRepeat -int 1
 # defaults write -g InitialKeyRepeat -int 10
 # Linux
-rm $HOME/.config/Code/User/settings.json
 rm $HOME/.config/Code/User/keybindings.json
-ln -s $CONFIG_HOME/vscode/settings.json $HOME/.config/Code/User/settings.json
+rm $HOME/.config/Code/User/settings.json
+
+ln -s $CONFIG_HOME/k9s/mocha.yml ~/.config/k9s/skin.yml
 ln -s $CONFIG_HOME/vscode/keybindings.json $HOME/.config/Code/User/keybindings.json
+ln -s $CONFIG_HOME/vscode/settings.json $HOME/.config/Code/User/settings.json
